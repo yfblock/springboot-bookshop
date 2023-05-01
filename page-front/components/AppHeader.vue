5563030c1ff4325749343295745dbd4af85eb9ee
yufeng <321353225@qq.com>
2023-05-01 21:57:50 +0800

    use types as menu

--- page-front/components/AppHeader.vue
+++ page-front/components/AppHeader.vue
@@ -54,31 +54,12 @@
 
         <!-- dropdown -->
         <div
-          class="absolute w-full left-0 top-full bg-white shadow-md py-3 divide-y divide-gray-300 divide-dashed opacity-0 group-hover:opacity-100 transition duration-300 invisible group-hover:visible">
-          <a href="#" class="flex items-center px-6 py-3 hover:bg-gray-100 transition">
-            <img src="/assets/images/icons/sofa.svg" alt="sofa" class="w-5 h-5 object-contain">
-            <span class="ml-6 text-grahtmly-600 text-sm">Sofa</span>
-          </a>
-          <a href="#" class="flex items-center px-6 py-3 hover:bg-gray-100 transition">
-            <img src="/assets/images/icons/terrace.svg" alt="terrace" class="w-5 h-5 object-contain">
-            <span class="ml-6 text-gray-600 text-sm">Terarce</span>
-          </a>
-          <a href="#" class="flex items-center px-6 py-3 hover:bg-gray-100 transition">
-            <img src="/assets/images/icons/bed.svg" alt="bed" class="w-5 h-5 object-contain">
-            <span class="ml-6 text-gray-600 text-sm">Bed</span>
-          </a>
-          <a href="#" class="flex items-center px-6 py-3 hover:bg-gray-100 transition">
-            <img src="/assets/images/icons/office.svg" alt="office" class="w-5 h-5 object-contain">
-            <span class="ml-6 text-gray-600 text-sm">office</span>
-          </a>
-          <a href="#" class="flex items-center px-6 py-3 hover:bg-gray-100 transition">
-            <img src="/assets/images/icons/outdoor-cafe.svg" alt="outdoor" class="w-5 h-5 object-contain">
-            <span class="ml-6 text-gray-600 text-sm">Outdoor</span>
-          </a>
-          <a href="#" class="flex items-center px-6 py-3 hover:bg-gray-100 transition">
-            <img src="/assets/images/icons/bed-2.svg" alt="Mattress" class="w-5 h-5 object-contain">
-            <span class="ml-6 text-gray-600 text-sm">Mattress</span>
-          </a>
+          class="absolute w-full left-0 top-full bg-white shadow-md pt-3 divide-y divide-gray-300 divide-dashed opacity-0 group-hover:opacity-100 transition duration-300 invisible group-hover:visible">
+          <div v-for="(item, index) in goodTypes">
+            <a href="#" class="flex items-center px-6 py-3 hover:bg-gray-100 transition bg-white">
+              <span class="ml-6 text-grahtmly-600 text-sm">{{ item['typeName'] }}</span>
+            </a>
+          </div>
         </div>
       </div>
 
@@ -95,5 +76,20 @@
 
 <script setup lang="ts">
 import { useUser } from '~/stores/user';
+import { useToast } from '~/stores/toast';
+
 const user = useUser();
+const toast = useToast();
+
+let goodTypes = ref([]);
+
+async function fetchGoodTypes() {
+    let res = await getGoodTypes();
+    if (res && res['status']) {
+        console.log(res['data'])
+        goodTypes.value = res['data'];
+    }
+}
+
+await fetchGoodTypes();
 </script>
